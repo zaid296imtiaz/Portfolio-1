160e9bbf5a84e5f30085dbb10dd594c72ea815f2
Zaid Imtiaz <zaid296imtiaz@gmail.com>
2024-04-13 19:01:42 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-const Layout = dynamic(() => import("@/components/Layout"), { ssr: false });
+// const Layout = dynamic(() => import("@/components/Layout"), { ssr: false });
+
+import Layout from "@/components/Layout";
 
 export default function page() {
   return <Layout />;
